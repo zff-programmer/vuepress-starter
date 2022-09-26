1d30a33aa99476b69516cf0c8340b9de7c52a8a7
zff <1290810710@qq.com>
2022-09-26 14:45:08 +0800

    update deploy.yml

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   base: '/vuepress-starter/', // 基路径
   // base: '/', // 基路径
-  dest: './dist', // 输出目录
+  // dest: './dist', // 输出目录
   host: '0.0.0.0',
   port: '9000',
   head: [['link', { rel: 'icon', href: '/images/hero.png' }]], // head标签里的logo
